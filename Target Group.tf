c8d8fb534c11a1d0bd75d20b521f361a7701a83f
prasad <dprasad28062001@gmail.com>
2026-06-06 20:15:36 +0100

    fix: change target group type to ip for ECS Fargate compatibility

--- Target Group.tf
+++ Target Group.tf
@@ -1,21 +1,23 @@
 resource "aws_lb_target_group" "web_target_group" {
-  name        = "private-webserver-target-group"
-  port        = 80
+  name        = "singingai-target-group"
+  port        = 3000
   protocol    = "HTTP"
   vpc_id      = aws_vpc.vpc.id
-  target_type = "instance"
+  target_type = "ip"
 
   health_check {
-    path                = "/healthcheck"
+    enabled             = true
+    path                = "/api/health"
+    port                = "traffic-port"
     protocol            = "HTTP"
-    port                = "80"
-    interval            = 30
-    timeout             = 5
     healthy_threshold   = 2
-    unhealthy_threshold = 2
+    unhealthy_threshold = 3
+    timeout             = 10
+    interval            = 30
+    matcher             = "200"
   }
 
   tags = {
-    Name = "private-webserver-target-group"
+    Name = "singingai-target-group"
   }
 }
