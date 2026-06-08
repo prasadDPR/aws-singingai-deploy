2e75bfe409096282fb17512875f8f9d0ad8d9161
prasad <dprasad28062001@gmail.com>
2026-06-08 12:43:25 +0100

    fix: change RDS port to 5432 PostgreSQL, allow ECS sg access

--- Security Group.tf
+++ Security Group.tf
@@ -1,8 +1,6 @@
 resource "aws_security_group" "public-sg" {
   vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "public-securitygroup"
-  }
+  tags   = { Name = "public-securitygroup" }
 
   ingress {
     description = "SSH"
@@ -22,9 +20,7 @@ resource "aws_security_group" "public-sg" {
 
 resource "aws_security_group" "private-sg" {
   vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "private-securitygroup"
-  }
+  tags   = { Name = "private-securitygroup" }
 
   ingress {
     from_port       = 22
@@ -32,7 +28,6 @@ resource "aws_security_group" "private-sg" {
     protocol        = "tcp"
     security_groups = [aws_security_group.public-sg.id]
   }
-
 
   ingress {
     from_port       = 80
@@ -51,14 +46,25 @@ resource "aws_security_group" "private-sg" {
 
 resource "aws_security_group" "alb_sg" {
   vpc_id = aws_vpc.vpc.id
-
-  tags = {
-    Name = "webserver-alb-sg"
-  }
+  tags   = { Name = "webserver-alb-sg" }
 
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -74,17 +80,26 @@ resource "aws_security_group" "alb_sg" {
 resource "aws_security_group" "rds-sg" {
   name   = "rds-security-group"
   vpc_id = aws_vpc.vpc.id
+  tags   = { Name = "rds-securitygroup" }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-resource "aws_security_group_rule" "allow_private_vm_ingress_to_rds" {
+# PostgreSQL from ECS tasks
+resource "aws_security_group_rule" "allow_ecs_to_rds" {
   type                     = "ingress"
-  from_port                = 3306
-  to_port                  = 3306
+  from_port                = 5432
+  to_port                  = 5432
   protocol                 = "tcp"
-  source_security_group_id = aws_security_group.private-sg.id
+  source_security_group_id = aws_security_group.ecs_sg.id
   security_group_id        = aws_security_group.rds-sg.id
+  description              = "Allow ECS tasks to connect to PostgreSQL"
 }
 
-
-
-
+# Remove old MySQL rule
+# resource "aws_security_group_rule" "allow_private_vm_ingress_to_rds" - DELETED
