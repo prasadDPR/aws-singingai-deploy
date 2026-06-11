fe9f90d4223af7059aabef370faa7d04dfcd5992
prasad <dprasad28062001@gmail.com>
2026-06-11 10:48:43 +0100

    fix: enable DNS hostnames and support on VPC for service discovery
    - enable_dns_hostnames required for Route53 private hosted zones
    - enable_dns_support required for VPC DNS resolution
    - Without these, service discovery DNS (singingai.local) never resolves

--- vpc.tf
+++ vpc.tf
@@ -1,72 +1,10 @@
 resource "aws_vpc" "vpc" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
     Name = "production-vpc"
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
