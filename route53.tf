aea5888f7f6d78e4ec35cdf23b8e6bc9b28fa9f4
prasad <dprasad28062001@gmail.com>
2026-06-12 07:35:05 +0100

    feat: auto DNS switch to maintenance page on destroy

--- route53.tf
+++ route53.tf
@@ -15,6 +15,15 @@ resource "aws_route53_record" "singingai" {
   }
 }
 
+# Always pointing to S3 static page
+resource "aws_route53_record" "maintenance" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "maintenance.prasadcloud.com"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["${aws_s3_bucket.static_fallback.bucket}.s3-website.eu-west-2.amazonaws.com"]
+}
+
 output "singingai_url" {
   value       = "https://singingai.prasadcloud.com"
   description = "SingingAI production URL"
